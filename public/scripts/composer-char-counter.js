f983a354551de180b299ae115407ddd45222fffa
Julia Gatina <julia.sarandi@gmail.com>
2021-12-15 07:56:58 +0000

    Add tweeter container section, add basic structure and create css file for it.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,24 +1,22 @@
 $(document).ready(function() {
-  
-  
-const $tweetText = $('#tweet-text');
-const $tweetCounter = $('#tweet-counter');
-const $button = $('#tweet-button');
 
+  const $tweetText = $('#tweet-text');
+  const $tweetCounter = $('.counter');
+  const $button = $('#tweet-button');
 
-$tweetText.on('keypress', function (event) {
-  const $maxValue = 140; 
-  const $currentValue = event.target.value.length;
-  console.log($currentValue)
-  const amountLeft = $maxValue - $currentValue;
+  // function to count char being entered, when > 140 -> turn counter red
+  $tweetText.on('keypress', function(event) {
+    const $maxValue = 140;
+    const $currentValue = event.target.value.length;
+    console.log($currentValue)
+    const amountLeft = $maxValue - $currentValue;
 
-  if (amountLeft < 0) {
-    $tweetCounter.addClass("red-counter");
-  }
-  
-  $tweetCounter.val(amountLeft);
+    if (amountLeft < 0) {
+      $tweetCounter.addClass('red-counter');
+    }
 
-})
+    $tweetCounter.val(amountLeft);
+
+  })
 
 });
-
