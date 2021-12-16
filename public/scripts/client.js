095cbc0652abb84d02f38642b118b417c8c73806
Julia Gatina <julia.sarandi@gmail.com>
2021-12-16 23:26:51 +0000

    Implement rederTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,21 +6,54 @@
 
 $(document).ready(function() {
 
-const userDataObject = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 
+/**
+ * takes in an array of tweet objects, calls createTweetElement for each tweet and appends each one to the #tweets-container.
+ * @param {*} tweets
+ * @returns createdTweet
+ */
+const renderTweets = function(tweets) {
+  let createdTweet;
+  // loops through tweets and createTweetElement for each tweet
+  for (const tweet of tweets) {
+    // takes return value and appends it to the tweets container
+    createdTweet = $('#tweets-container').append(createTweetElement(tweet));
+  }
+  return createdTweet;
+}
+
+/**
+ * takes in single tweet object and creates a markup that will be used to render tweets dynamically
+ * @param {*} single tweetObj 
+ * @returns tweet markup
+ */
 const createTweetElement = function(tweetObj) {
-  const $tweet = $(`
+  let $tweet = $(`
   <article class="tweet">
     <header>
       <div class="username">
@@ -48,9 +81,6 @@ const createTweetElement = function(tweetObj) {
   return $tweet;
 }
 
-
-  const createdTweet = createTweetElement(userDataObject);
-  $('#tweets-container').append(createdTweet);
-
+renderTweets(data);
 
 });
